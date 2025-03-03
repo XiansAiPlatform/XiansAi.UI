b0760e02a77a9602ba71a8492f7841e6d334a903
Hasith Yaggahavita <hasith@gmail.com>
2025-03-03 19:39:27 +0100

    Swap order of inputs and result in ActivityDetailsView

--- src/components/WorkflowDetails/ActivityDetailsView.jsx
+++ src/components/WorkflowDetails/ActivityDetailsView.jsx
@@ -4,21 +4,21 @@ import JsonViewer from '../JsonViewer/JsonViewer';
 
 const ActivityDetailsView = ({ activityDetails }) => (
   <Box>
-    {activityDetails.inputs && (
-      <>
-        <Typography variant="h6" sx={{ mt: 3 }}>
-          Inputs:
-        </Typography>
-        <JsonViewer data={activityDetails.inputs} initialDepth={2} />
-      </>
-    )}
-
     {activityDetails.result && (
       <>
         <Typography variant="h6" sx={{ mt: 3 }}>
           Output:
         </Typography>
         <JsonViewer data={activityDetails.result} initialDepth={2} />
+      </>
+    )}
+    
+    {activityDetails.inputs && (
+      <>
+        <Typography variant="h6" sx={{ mt: 3 }}>
+          Inputs:
+        </Typography>
+        <JsonViewer data={activityDetails.inputs} initialDepth={2} />
       </>
     )}
   </Box>
