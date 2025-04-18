1d6a8da4e74a789efc7fdbc4c294dddcba3e0d93
Hasith Yaggahavita <hasith@gmail.com>
2025-04-18 22:01:21 +0530

    Remove unused icon imports from LeftNav component to streamline code and improve maintainability.

--- src/components/Layout/LeftNav.jsx
+++ src/components/Layout/LeftNav.jsx
@@ -5,11 +5,6 @@ import AirIcon from '@mui/icons-material/Air';
 import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
 import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
 import { Link, useLocation } from 'react-router-dom';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import SettingsIcon from '@mui/icons-material/Settings';
-import DescriptionIcon from '@mui/icons-material/Description';
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
-import ListAltIcon from '@mui/icons-material/ListAlt';
 import ForumOutlinedIcon from '@mui/icons-material/ForumOutlined';
 
 const NAV_ITEMS = [
