e5d99c131508dc33261c47c2217deb85b2eaf8ff
Hasith Yaggahavita <hasith@gmail.com>
2025-05-16 23:37:12 +0200

    merge of the auditing page

--- src/modules/Manager/ManagerRoutes.jsx
+++ src/modules/Manager/ManagerRoutes.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { NotificationProvider } from './contexts/NotificationContext';
+import { ErrorNotificationProvider } from './contexts/ErrorNotificationContext';
 import WorkflowList from './Components/Runs/WorkflowList';
 import WorkflowDetails from './Components/Runs/WorkflowDetails/WorkflowDetails';
 import Layout from './Components/Layout/Layout';
@@ -20,6 +21,8 @@ import ProtectedRoute from './auth/ProtectedRoute';
 import { useAuth0 } from '@auth0/auth0-react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import AuditingPage from './Components/Auditing/AuditingPage';
+
 
 function ManagerRoutes() {
   const { logout, isLoading, error} = useAuth0();
@@ -45,54 +48,61 @@ function ManagerRoutes() {
   return (
     <NotificationProvider>
       <OrganizationProvider>
-        <ThemeProvider theme={theme}>
-          <ToastContainer />
-          <LoadingProvider>
-            <SliderProvider>
-              <Toaster />
-              <Routes>
-                <Route path="/logout" element={<LogoutHandler onLogout={handleLogout} />} />
-                <Route element={<Layout />}>
-                  <Route path="/runs" element={
-                    <ProtectedRoute>
-                      <WorkflowList />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/definitions" element={
-                    <ProtectedRoute>
-                      <DefinitionList />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/runs/:id/:runId" element={
-                    <ProtectedRoute>
-                      <WorkflowDetails />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/agents" element={
-                    <ProtectedRoute>
-                      <NotImplemented />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/knowledge" element={
-                    <ProtectedRoute>
-                      <Knowledge />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/settings" element={
-                    <ProtectedRoute>
-                      <Settings />
-                    </ProtectedRoute>
-                  } />
-                  <Route path="/messaging" element={
-                    <ProtectedRoute>
-                      <MessagingPage />
-                    </ProtectedRoute>
-                  } />
-                </Route>
-              </Routes>
-            </SliderProvider>
-          </LoadingProvider>
-        </ThemeProvider>
+        <ErrorNotificationProvider>
+          <ThemeProvider theme={theme}>
+            <ToastContainer />
+            <LoadingProvider>
+              <SliderProvider>
+                <Toaster />
+                <Routes>
+                  <Route path="/logout" element={<LogoutHandler onLogout={handleLogout} />} />
+                  <Route element={<Layout />}>
+                    <Route path="/runs" element={
+                      <ProtectedRoute>
+                        <WorkflowList />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/definitions" element={
+                      <ProtectedRoute>
+                        <DefinitionList />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/runs/:id/:runId" element={
+                      <ProtectedRoute>
+                        <WorkflowDetails />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/agents" element={
+                      <ProtectedRoute>
+                        <NotImplemented />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/knowledge" element={
+                      <ProtectedRoute>
+                        <Knowledge />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/settings" element={
+                      <ProtectedRoute>
+                        <Settings />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/messaging" element={
+                      <ProtectedRoute>
+                        <MessagingPage />
+                      </ProtectedRoute>
+                    } />
+                    <Route path="/auditing" element={
+                      <ProtectedRoute>
+                        <AuditingPage />
+                      </ProtectedRoute>
+                    } />
+                  </Route>
+                </Routes>
+              </SliderProvider>
+            </LoadingProvider>
+          </ThemeProvider>
+        </ErrorNotificationProvider>
       </OrganizationProvider>
     </NotificationProvider>
   );
