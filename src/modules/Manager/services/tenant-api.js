1eceaf28a374bdc21e6399c1bb248b8546e5322e
pasindu-2002 <pasindualuthwalahewa@gmail.com>
2025-06-02 18:48:13 +0530

    update tenant api service

--- src/modules/Manager/services/tenant-api.js
+++ src/modules/Manager/services/tenant-api.js
@@ -5,40 +5,39 @@ export const useTenantApi = () => {
     const apiClient = useApiClient();
     
     return useMemo(() => {
-        return {
-
-        getTenant: async (tenantId) => {
-            if (!tenantId) {
-                console.warn('No tenant ID provided to getTenant');
-                return null;
-            }
+        return {            
+              getTenant: async (tenantId) => {
+                if (!tenantId) {
+                    console.warn('No tenant ID provided to getTenant');
+                    return null;
+                }
+                
+                try {
+                    return apiClient.get(`/api/client/tenants/by-tenant-id/${tenantId}`);
+                } catch (error) {
+                    console.error(`Error fetching tenant ${tenantId}:`, error);
+                    return null;
+                } 
+            },
             
-            try {
-                return await apiClient.get('/api/client/tenants/', tenantId);
-            } catch (error) {
-                console.error(`Error fetching tenant ${tenantId}:`, error);
-                return null;
-            } 
-        },
+            updateTenant: async (tenantId, tenantData) => {
+                try {
+                    console.log('Update Data:', tenantData);
+                    return await apiClient.put(`/api/client/tenants/${tenantId}`, tenantData);
+                } catch (error) {
+                    console.error('Error updating tenant:', error);
+                    return null;
+                }
+            },
         
-        updateTenant: async (tenantId, tenantData) => {
-            try {
-                console.log('Update Data:', tenantData);
-                return await apiClient.put(`/api/client/tenants/${tenantId}`, tenantData);
-            } catch (error) {
-                console.error('Error updating tenant:', error);
+            deleteTenant: async (tenantId) => {
+                try {
+                return await apiClient.delete(`/api/client/tenants/${tenantId}`);
+                } catch (error) {
+                console.error('Error deleting tenant:', error);
                 return null;
-            }
-        },
-    
-        deleteTenant: async (tenantId) => {
-            try {
-            return await apiClient.delete(`/api/client/tenants/${tenantId}`);
-            } catch (error) {
-            console.error('Error deleting tenant:', error);
-            return null;
-            }
-        },
+                }
+            },
         };
     }, [apiClient]);
     }
