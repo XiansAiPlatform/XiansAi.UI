277e53ed423bc17c767a86c548398a19d034e008
pasindu-2002 <pasindualuthwalahewa@gmail.com>
2025-06-06 08:27:30 +0530

    resolve the conflicts

--- src/modules/Manager/Components/Settings/Settings.jsx
+++ src/modules/Manager/Components/Settings/Settings.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Box, Typography, Container, Alert, Tabs, Tab } from '@mui/material';
 import CACertificates from './AppServerSettings';
 import TenantSettings from './TenantSettings';
+import BrandingSettings from './BrandingSettings';
 import './Settings.css';
 
 const Settings = () => {
@@ -40,12 +41,15 @@ const Settings = () => {
           >
             <Tab label="App Server" />
             <Tab label="Tenants" />
+            <Tab label="Branding" />
           </Tabs>
         </Box>
 
         <Box role="tabpanel">
           {currentTab === 0 && <CACertificates />}
           {currentTab === 1 && <TenantSettings />}
+          {currentTab === 1 && <BrandingSettings />}
+
         </Box>
       </Box>
     </Container>
